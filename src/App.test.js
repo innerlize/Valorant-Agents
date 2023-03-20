50405ac797de0f084b9eb86fdd7b333ab1bffe9f
Alan <tiltedveggie@outlook.kr>
2023-03-20 14:01:46 -0300

    Fixing: Cannot use import statement outside a module when importing Axios

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders title', () => {
 	render(<App />);
-	const linkElement = screen.getByText(/hello :d/i);
-	expect(linkElement).toBeInTheDocument();
+	const titleElement = screen.getByText(/valorant agents/i);
+	expect(titleElement).toBeInTheDocument();
 });
