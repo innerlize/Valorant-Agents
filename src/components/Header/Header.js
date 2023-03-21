67148bff71db36b97ff94b78dfc7faa775438fc6
Alan <tiltedveggie@outlook.kr>
2023-03-21 13:46:19 -0300

    Adding styles to the header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
 	return (
-		<header>
-			<div>
-				<img src={require('../../assets/Logo.png')} />
+		<header id='header'>
+			<div className='header__logo-container'>
+				<img src={require('../../assets/Logo.png')} className='logo' />
 			</div>
 
-			<div>
-				<h1>VAlorant Agents</h1>
-				<h2>
+			<div className='header__titles-container'>
+				<h1 className='header__title'>VAlorant Agents</h1>
+				<h2 className='header__subtitle'>
 					Created by{' '}
 					<a
 						href='https://tiltedveggie.github.io/Alan-Aguilar/'
 						target='_blank'
-						rel='noreferrer'>
+						rel='noreferrer'
+						className='header__link'>
 						Alan
 					</a>
 				</h2>
