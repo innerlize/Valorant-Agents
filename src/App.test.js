31b33f18eed02ffd69cd52ae61b00a72403f52de
Alan <tiltedveggie@outlook.kr>
2023-03-21 22:36:34 -0300

    Add Header Section tests

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Renders title', () => {
-	render(<App />);
-	const titleElement = screen.getByText(/valorant agents/i);
-	expect(titleElement).toBeInTheDocument();
-});
---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Header from './Header.js';
+
+test('Renders title', () => {
+	render(<Header />);
+
+	const logoElement = screen.getByAltText(/logo/i);
+
+	const titleElement = screen.getByText(/valorant agents/i);
+	const subtitleElement = screen.getByTestId('subtitle');
+
+	expect(logoElement).toBeInTheDocument();
+	expect(titleElement).toBeInTheDocument();
+	expect(subtitleElement.textContent).toBe('Created by Alan');
+});
