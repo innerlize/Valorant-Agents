34932faaa97257c4ca20003e52bee2ae2c7d4beb
Alan <tiltedveggie@outlook.kr>
2023-03-24 17:51:32 -0300

    Add styles to the Agents Container Section

--- src/components/AgentsContainer/AgentBox/AgentBox.js
+++ src/components/AgentsContainer/AgentBox/AgentBox.js
@@ -3,19 +3,27 @@ import './AgentBox.css';
 
 function AgentBox(agent) {
 	return (
-		<div data-testid='agent-card'>
-			<div>
-				<div>
-					<img src={agent.fullPortrait} alt={`${agent.displayName} Portrait`} />
+		<div className='agent-box' data-testid='agent-card'>
+			<div className='agent-box__content-wrapper'>
+				<div className='agent-box__image-wrapper'>
+					<img
+						src={agent.fullPortrait}
+						className='agent-box__image'
+						alt={`${agent.displayName} Portrait`}
+					/>
 				</div>
-				<div>
-					<div>
-						<p role='agent-name'>{agent.displayName}</p>
+				<div className='agent-box__text-wrapper'>
+					<div className='agent-box__name-wrapper'>
+						<p className='agent-box__name' role='agent-name'>
+							{agent.displayName}
+						</p>
 					</div>
-					<button>
-						<span>
-							<span></span>
-							<span role='agent-button'>Read More</span>
+					<button className='agent-box__button button'>
+						<span className='button__inner'>
+							<span className='button__slide'></span>
+							<span className='button__content' role='agent-button'>
+								Read More
+							</span>
 						</span>
 					</button>
 				</div>
--- src/components/AgentsContainer/AgentsContainer.js
+++ src/components/AgentsContainer/AgentsContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useFetch } from '../../hooks/useFetch.js';
 import AgentBox from './AgentBox/AgentBox.js';
+import './AgentsContainer.css';
 
 function AgentsContainer() {
 	const { agents, loading, error } = useFetch(
@@ -8,12 +9,14 @@ function AgentsContainer() {
 	);
 
 	return (
-		<section>
-			<div>
-				{error && <p>Error: {error.message}</p>}
+		<section id='agents-container'>
+			<div className='agents-container__wrapper'>
+				{error && (
+					<p className='agents-container__error'>Error: {error.message}</p>
+				)}
 
 				{loading && (
-					<div>
+					<div className='agents-container__loading'>
 						<img
 							src={require('../../assets/Tactibear.gif')}
 							alt='Tactibear GIF'
