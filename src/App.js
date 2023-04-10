bce369d0623b355c5019356ed628570631115595
Alan <tiltedveggie@outlook.kr>
2023-04-10 02:37:38 -0300

    Enclose components inside the  Context Provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from './components/Header/Header.js';
 import AgentsContainer from './components/AgentsContainer/AgentsContainer.js';
+import AgentInformationPanel from './components/AgentInformationPanel/AgentInformationPanel.js';
+import { PanelContextProvider } from './contexts/Panel.context.js';
 import './App.css';
 
 function App() {
 	return (
 		<main>
 			<Header />
-			<AgentsContainer />
+			<PanelContextProvider>
+				<AgentsContainer />
+				<AgentInformationPanel />
+			</PanelContextProvider>
 		</main>
 	);
 }
